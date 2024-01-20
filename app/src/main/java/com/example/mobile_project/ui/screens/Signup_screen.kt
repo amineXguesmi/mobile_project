acf34c97eee2a27954d28e809d0a518a3fc1b349
amine <mohamedamineguesmi@insat.u-carthage.tn>
2024-01-20 16:48:37 +0100

    sign up

--- app/src/main/java/com/example/mobile_project/ui/screens/Signup_screen.kt
+++ app/src/main/java/com/example/mobile_project/ui/screens/Signup_screen.kt
@@ -2,11 +2,17 @@ package com.example.mobile_project.ui.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.mobile_project.R
 
 class SignUpScreen : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_screen)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
